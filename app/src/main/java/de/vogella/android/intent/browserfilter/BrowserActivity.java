038cb0548502fc17abd6174767f81560e8903ca6
rduncan354 <rduncan354@gmail.com>
2016-11-08 16:08:41 -0500

    Corrected problem with manifest

--- app/src/main/java/de/vogella/android/intent/browserfilter/BrowserActivity.java
+++ app/src/main/java/de/vogella/android/intent/browserfilter/BrowserActivity.java
@@ -1,13 +1,50 @@
 package de.vogella.android.intent.browserfilter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.TextView;
 
-public class BrowserActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+public class BrowserActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_browser);
+
+        // To keep this example simple, we allow network access
+        // in the user interface thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        TextView text = (TextView) findViewById(R.id.textView);
+        // To get the action of the intent use
+        String action = intent.getAction();
+        if (!action.equals(Intent.ACTION_VIEW)) {
+            throw new RuntimeException("Should not happen");
+        }
+        // To get the data use
+        Uri data = intent.getData();
+        URL url;
+        try {
+            url = new URL(data.getScheme(), data.getHost(), data.getPath());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(
+                    url.openStream()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                text.append(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
